ebedb6ec1de8bf0fdcae66c31e6959a896430530
ILYA <ilya_kurmaev@mail.ru>
2020-06-11 23:21:37 +0400

    Реализовано подключение и отключеник к кассе через TCP.

--- app/src/main/java/com/example/shtrih/Kassa.java
+++ app/src/main/java/com/example/shtrih/Kassa.java
@@ -1,8 +1,12 @@
 package com.example.shtrih;
 
+import android.widget.EditText;
+import android.widget.TextView;
+
 import ru.shtrih_m.fr_drv_ng.classic_interface.classic_interface;
 
 final public class Kassa  {
+
 
     public static classic_interface device = new classic_interface();
 
@@ -20,4 +24,22 @@ final public class Kassa  {
     {
         device.PrintReportWithoutCleaning();
     }
+
+    public static void FrConnect(String address, int port, int timeout)            //Подключить кассу
+    {
+        Kassa.device.Set_ConnectionType(classic_interface.TConnectionType.Tcp);
+        Kassa.device.Set_UseIPAddress(true);
+        Kassa.device.Set_IPAddress(address);
+        Kassa.device.Set_TCPPort(port);
+        Kassa.device.Set_Timeout(timeout);
+        Kassa.device.Set_ProtocolType(0);
+        Kassa.device.Set_Password(30);
+    }
+
+    public static void FrDisconnect()   // Отключить кассу
+    {
+        Kassa.device.Disconnect();
+        int answer = Kassa.device.Connect();
+    }
+
 }
